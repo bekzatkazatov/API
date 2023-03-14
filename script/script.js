6572feb7d5a5fc512aee64a8b03dd23b84687e32
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-14 14:31:04 +0600

    api

---
+++ script/script.js
@@ -0,0 +1,23 @@
+// id=1
+// fetch(`https://swapi.dev/api/people/ ${id}`)
+//
+//   .then  ( res => res.json())
+//   .then ( (data) => alert(data.name +' '+ data.mass +' '+ data.vehicles))
+
+// id=6
+// fetch(`https://swapi.dev/api/people/ ${id}`)
+//
+//     .then  ( res => res.json())
+//     .then ( (data) => alert(`name${data.name},age:${data.birth_year},gender:${data.gender} `))
+
+// id=5
+// fetch(`https://swapi.dev/api/people/ ${id}`)
+//
+//     .then  ( res => res.json())
+//     .then ( (data) => alert(`name: ${data.name},hair: ${data.hair_color},vehicles: ${data.vehicles}`))
+
+// id=3
+// fetch(`https://swapi.dev/api/people/ ${id}`)
+//
+//     .then  ( res => res.json())
+//     .then ( (data) => alert(`name: ${data.name},birth: ${data.birth_year},starships: ${data.starships}`))
