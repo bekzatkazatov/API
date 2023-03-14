cbe9baf399f6fd0b0b0671738979b8d77afaf588
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-14 14:43:59 +0600

    api

--- script/script.js
+++ script/script.js
@@ -21,3 +21,15 @@
 //
 //     .then  ( res => res.json())
 //     .then ( (data) => alert(`name: ${data.name},birth: ${data.birth_year},starships: ${data.starships}`))
+
+// id=9
+// fetch(`https://swapi.dev/api/starships/ ${id}`)
+//
+//     .then  ( res => res.json())
+//     .then ( (data) => alert(`name: ${data.name},model: ${data.model},consumables: ${data.consumables}`))         name: Death Star,model: DS-1 Orbital Battle Station,consumables: 3 years
+
+// id=3
+// fetch(`https://swapi.dev/api/planets/ ${id}`)
+//
+//     .then  ( res => res.json())
+//     .then ( (data) => alert(`name: ${data.name},rotation_period: ${data.rotation_period},diameter: ${data.diameter}`))        name: Yavin IV,rotation_period: 24,diameter: 10200
